b5e3747b9ba3dee9a24ea047fe5c9b505c5c50b3
Ngọc Lương <luongngoc590@gmail.com>
2025-07-04 06:42:53 +0000

    làm sao để tôi biết đc chính xác lí do k tải đđc, liệu có phải di file m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1a110f" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,400..900;1,7..72,400..900&display=swap" rel="stylesheet" />

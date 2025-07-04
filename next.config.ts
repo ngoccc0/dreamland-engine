e2060dbb9a70f061ff797adb99987e5b5819e462
Ngọc Lương <luongngoc590@gmail.com>
2025-07-04 05:07:01 +0000

    khi tải về đã cho app lưu trữ cục bộ chưa ?

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,19 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+        },
+      },
+    },
+  ],
 });
 
 

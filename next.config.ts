623f11c745c9adfbe812a4c9e86d0626c9c980e8
Ngọc Lương <luongngoc590@gmail.com>
2025-07-06 07:48:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
-  // Forcing PWA in dev mode to make testing easier.
-  // For a real project, it's recommended to disable in dev to avoid caching issues.
-  disable: false, 
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
   runtimeCaching: [
